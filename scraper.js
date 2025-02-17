7fa15c7deb1f5a906d9569a18f91674ae86479c2
Lasse <lasse.andresen9@gmail.com>
2025-02-17 19:36:35 +0100

    Cleanup

--- scraper.js
+++ scraper.js
@@ -6,81 +6,43 @@
 const cheerio = require('cheerio'); // For light weight scraping
 const puppeteer = require('puppeteer'); // For more advanced scraping
 
-// scrape_cheerio();
-myScraper('h1[data-type="product"]', 'span.site-currency-attention');
+const url = 'https://www.proshop.dk/Mus/Logitech-G903-LIGHTSPEED-HERO-Wireless-Gaming-Mus-Optisk-11-knapper-Sort-med-RGB-lys/2778872';
+const productSelector = 'h1[data-type="product"]';
+const priceSelector = 'span.site-currency-wrapper > span.site-currency-attention';
+scrape(url, [productSelector, priceSelector])
+  .then(result => console.log('Result ', result));
 
-async function scrape_cheerio() {
-  const url = 'https://www.example.com';
-  const response = await fetch(url);
 
-  const $ = cheerio.load(await response.text());
-  var header = $('h1').text();
-  var text = $('p').text();
-  var link = $('a').attr('href');
-  console.log(header);
-  console.log(text);
-  console.log(link);
-};
-
-async function scrape_puppeteerAndCheerio() {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  
-  await page.goto('https://quotes.toscrape.com/');
-  
-  await browser.close();
-
-  const pageData = await page.evaluate(() => {
-    return {
-      html: document.documentElement.innerHTML,
-    };
-  });
-  console.log('Page data ', pageData);
-  	
-  const $ = cheerio.load(pageData.html);
-}
-
-async function myScraper(productSelector, priceSelector) {
+async function scrape(url, selectors) {
   const result = [];
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
-  await page.goto('https://www.proshop.dk/Mus/Logitech-G903-LIGHTSPEED-HERO-Wireless-Gaming-Mus-Optisk-11-knapper-Sort-med-RGB-lys/2778872');
-  
-  try {
-    await page.waitForSelector('span.site-currency-wrapper', {timeout: 5000});
-    await page.waitForSelector(productSelector, {timeout: 5000});
-    // await new Promise((resolve, reject) => { setTimeout(resolve, 5000)});
-  } catch (e) {
-    console.error('Price element not found after 5 seconds');
-    browser.close();
-    return;
-  }
-  
+  await page.goto(url);
+
   const pageData = await page.evaluate(() => {
     return {
       html: document.documentElement.innerHTML,
     };
   });
   // console.log('Page data ', pageData);
-  	
-  const $ = cheerio.load(pageData.html);
-  // var spans = $('span.site-currency-wrapper');
-  var wrapper = $('span.site-currency-wrapper');
-  var productName = $(productSelector).text();
-  var productPrice = $(wrapper).find(priceSelector).text();
-  console.log('Product price: ', productPrice);
-  result.push({
-    name: productName,
-    price: productPrice
-  });
-
-  console.log('Found item ', result[0]);
+  
+  for (const selector of selectors) {
+    try {
+      await page.waitForSelector(selector, {timeout: 5000});
+    } catch (e) {
+      console.error('Selector not found after 5 seconds ', selector);
+      result.push(null);
+      continue;
+    }
+    
+    const $ = cheerio.load(pageData.html);
+    var text = $(selector).text();
+    result.push(text);
+  }
+  
   browser.close();
-}
 
-
-class scrapedItem {
-  name;
-  price;
+  console.log('Scraped from URL: ' + url + '\n', result);
+  return result;
 }
