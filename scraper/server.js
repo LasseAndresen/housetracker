c59fabed6dab999a1bbc98197b58cf322930a9b4
Lasse <lasse.andresen9@gmail.com>
2025-03-22 21:45:54 +0100

    Added scraper api to docker-compose

--- scraper/server.js
+++ scraper/server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const scrapeWebsite = require('./scraper');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3002;
 
 app.use(express.json());
 app.use(cors()); // Allow all origins to access
