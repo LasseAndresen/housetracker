478e7bb85a7bdcbd7fb2f2db2a0d0e4b8640a190
Lasse <lasse.andresen9@gmail.com>
2025-03-23 12:29:13 +0100

    Dockerized Angular ssr front end and added it to docker-compose

---
+++ housetracker/src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  featureToggle: {
+    myNewShinyFeature: false
+  }
+}
---
+++ housetracker/src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  backendUrl: 'http://localhost:3000',
+  scraperAPIUrl: 'http://localhost:3002',
+  featureToggle: {
+    myNewShinyFeature: true
+  }
+}
